fa79225ad4cba9d42d1a799800ef7da4ca31975c
Halú de Cássia <haluanedecassia@gmail.com>
2019-05-19 23:41:18 -0300

    Updates

---
+++ precache-manifest.ef3fe628918fe69e1e0570758f1948c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a050fffc81afbce5e7568ddc846efa90",
+    "url": "/spotify-view/index.html"
+  },
+  {
+    "revision": "f2bfdcde9ea3a5d10b11",
+    "url": "/spotify-view/static/css/main.2334483c.chunk.css"
+  },
+  {
+    "revision": "453ded4a47a1a1a2e733",
+    "url": "/spotify-view/static/js/2.774c5952.chunk.js"
+  },
+  {
+    "revision": "f2bfdcde9ea3a5d10b11",
+    "url": "/spotify-view/static/js/main.6074808e.chunk.js"
+  },
+  {
+    "revision": "60047f36b9b65f3c8b73",
+    "url": "/spotify-view/static/js/runtime~main.e124bd55.js"
+  }
+]);
